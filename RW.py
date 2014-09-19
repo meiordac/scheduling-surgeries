db0b569067d6e2ff4d4c459641fce7bcf4408244
meiordac <matias.iordache@gmail.com>
2014-09-19 07:22:15 -0300

    algunos cambios

--- RW.py
+++ RW.py
@@ -8,13 +8,11 @@ class ReadExcel:
   @staticmethod
   def read():
     rows = []
-    book = open_workbook('LE.xls')
+    book = open_workbook('Input/LE.xls')
     sheet = book.sheet_by_index(0)
  
     for row_index in range(1,sheet.nrows):
-      rows.append({"Rut":sheet.cell_value(row_index,0),"Nombre":sheet.cell_value(row_index,1),"Fecha":sheet.cell_value(row_index,2) })
+      rows.append({"Name":sheet.cell_value(row_index,0),"Date":sheet.cell_value(row_index,1),"Age":sheet.cell_value(row_index,2), "Rut":sheet.cell_value(row_index,3) })
 
     return rows
 
-rows = ReadExcel.read()
-print rows
---
+++ main.py
@@ -0,0 +1,8 @@
+import RW
+
+def main():
+  rows = RW.ReadExcel.read()
+  #rows = ReadExcel.read()
+  print rows
+if __name__ == "__main__":
+    main()
