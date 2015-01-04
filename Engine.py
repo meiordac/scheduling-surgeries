393cd4cfa968a19df65cc9aa84c31a7d4585f801
meiordac <matias.iordache@gmail.com>
2015-01-04 14:22:36 -0300

    minor changes

--- Engine.py
+++ Engine.py
@@ -60,7 +60,7 @@ class Engine:
   def Solve(self,pr):
     print "Solving problem..."
     #capture = subprocess.check_output(["glpsol", "--math", "Model/model.mod", "--data", "Model/model.dat", "--tmlim","60", "--write", "Output/write.out"])
-    subprocess.call(["glpsol", "--math", "Model/model.mod", "--data", "Model/model.dat", "--tmlim","3600", "--output", "Output/output.out", "--write", "Output/write.out"])
+    subprocess.call(["glpsol", "--math", "Model/model.mod", "--data", "Model/model.dat", "--tmlim","360", "--output", "Output/output.out", "--write", "Output/write.out"])
     #subprocess.call(["glpsol", "--math", "Model/model.mod", "--data", "Model/model.dat", "--output", "Output/output.out", "--write", "Output/write.out"])
     return pr.solution()
     
